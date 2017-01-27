a541d0fae4a7fa3df2def01e0ff269136e5db802
Tanner <tanner.crockett@gmail.com>
2017-01-27 16:29:19 -0700

    strings assignment

--- src/javascript/intro/grammer/strings/index.js
+++ src/javascript/intro/grammer/strings/index.js
@@ -1,1 +1,13 @@
-export const greeting = '';
+export const greeting = 'Hello World';
+
+export const characterAt = greeting.charAt(4);
+
+export const greetingArray = greeting.split('');
+
+export const begins = (str, char) => {
+  return (str[0] === char);
+};
+
+export const beginsInsensitive = (str, char) => {
+  return (str[0].toLowerCase() === char.toLowerCase());
+};
